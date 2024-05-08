28edd9ccb29ffe19899ab7ab9fd94b8781d49708
jonathan <jonathanjzsalazar@gmail.com>
2024-05-08 01:34:09 +0100

    Update add_new_todo, show_all_todos and delete_todo functions

--- src/main.py
+++ src/main.py
@@ -5,16 +5,29 @@ def main():
 
   def add_new_todo():
     print('Añadir una nueva tarea seleccionado')
+    new_todo_text = input('Tarea: ')
+    new_todo = {
+      'text': new_todo_text,
+      'is_completed': False
+    }
+    todo_list.append(new_todo)
 
   def show_all_todos():
     print('* Lista de tareas')
     if len(todo_list) == 0:
       print('\tNo hay tareas')
     else:
-      print('Alguna tarea hay - todo')
+      for index, todo in enumerate(todo_list):
+        print(f'\t{index} - {todo['text']} ({'completed' if todo['is_completed'] == True else 'not completed'})')
 
   def delete_todo():
     print('Borrar una tarea seleccionado')
+    try:
+      delete_todo_index = int(input('Introduce el indice de la tarea a eliminar: '))
+      todo_list.remove(todo_list[delete_todo_index])
+      print('La tarea se ha eliminado correctamente.')
+    except ValueError:
+      print('Ese indice no es correcto (No esta presente en la lista de tareas). Por favor introduce otro indice.')
 
   def exit():
     print('Salir de la aplicacion seleccionado')
@@ -22,13 +35,11 @@ def main():
 
   def show_options_menu():
     print('* Menu de opciones')
-    print('''
-\t1 - Agregar una nueva tarea
+    print('''\t1 - Agregar una nueva tarea
 \t2 - Mostrar todas las tareas
 \t3 - Eliminar una tarea
 \t4 - Mostrar menu de opciones
-\t5 - Salir.
-''')
+\t5 - Salir.''')
 
   # Cuando la app se ejecuta debo mostrar
   print('\t** Bienvenido/a **')
