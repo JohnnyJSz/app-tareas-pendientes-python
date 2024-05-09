dd6c6697bbbcba25e72c614d8000402f4d2a3e23
jonathan <jonathanjzsalazar@gmail.com>
2024-05-09 22:31:56 +0100

    Add auto incremental id for each instance of Todo

--- src/base.py
+++ src/base.py
@@ -1,7 +1,14 @@
 class Todo:
+  # Variable de clase para realizar un seguimiento del ID
+  last_id = 0
   def __init__(self, text, is_completed = False) -> None:
+    Todo.last_id += 1  # Incrementa el ID por cada nueva instancia
+    self.id = Todo.last_id # Asigna el nuevo ID a la instancia
     self.text = text
     self.is_completed = is_completed
+  
+  def get_id(self):
+    return self.id
   
   def get_todo(self):
     return self.text
